bbc5dfb381ebabc6a3e0e894c8a501cc3548fc83
David Garbutt <jayemen.85@gmail.com>
2016-11-06 22:40:23 -0500

    Add interpolation.

--- Data/Scripts/FlightPathMarker/FlightPathMarker.cs
+++ Data/Scripts/FlightPathMarker/FlightPathMarker.cs
@@ -15,24 +15,62 @@ namespace FlightPathMarker
     [MySessionComponentDescriptor(MyUpdateOrder.AfterSimulation)]
     public class FlightPathMarker : MySessionComponentBase
     {
-        const float WIDTH_ANGLE = 0.001f;
-        const float LENGTH_ANGLE = .005f;
-        const float CROSSHAIR_DISTANCE = .5f;
+        private const float WIDTH_ANGLE = 0.002f;
+        private const float LENGTH_ANGLE = 0.01f;
+        private const float CROSSHAIR_DISTANCE = 1.0f;
+        private const float SLERP_MS = 250;
+
+        private Vector3? lastOffset;
+        private long lastTick = 0;
 
         public override void Draw()
         {
+            var tick = DateTime.UtcNow.Ticks;
+            var tickDelta = tick - lastTick;
+
             var physics = ShipPhysics();
             if (physics == null)
             {
+                lastOffset = null;
                 return;
             }
             
+            
             var offset = physics.LinearVelocity;
+            if (offset.LengthSquared() == 0)
+            {
+                lastOffset = null;
+                return;
+            }
+
             offset.Normalize();
             offset *= CROSSHAIR_DISTANCE;
+            
+            if (lastOffset.HasValue)
+            {
+                // (100ns/tick) * (1 slerp / SLERP_MS) * (0.0001 ms / 100 ns) = (0.0001 ms / SLERP_MS) slerps/tick
+                var frac = 0.0001f / SLERP_MS * tickDelta;
 
+                Slerp(
+                    from: lastOffset.Value, 
+                    to: offset, 
+                    frac: frac, 
+                    output: ref offset);
+
+                offset.Normalize();
+            }
+
+            
             DrawCross(offset, Color.LightGreen);
             DrawCross(-offset, Color.Red);
+
+            lastTick = tick;
+            lastOffset = offset;
+        }
+
+        private void Slerp(Vector3 from, Vector3 to, float frac, ref Vector3 output)
+        {
+            output = (to - from) * frac + from;
         }
 
         private void DrawCross(Vector3 offset, Color color)
