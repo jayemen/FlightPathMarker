3ae545e732ad58a3c09e3e59154b13b42c605626
David Garbutt <jayemen.85@gmail.com>
2016-11-07 21:24:59 -0500

    Add ability to set frame of reference.

--- Data/Scripts/FlightPathMarker/FlightPathMarker.cs
+++ Data/Scripts/FlightPathMarker/FlightPathMarker.cs
@@ -1,8 +1,13 @@
 using Sandbox.ModAPI;
 using VRage.Game.Components;
 using VRageMath;
-using VRage.Game;
 using VRage.Game.ModAPI;
+using VRage.Input;
+using Sandbox.Game;
+using VRage.ModAPI;
+using VRage.Game;
+using VRage.Game.ModAPI.Interfaces;
+using System.Collections.Generic;
 
 namespace FlightPathMarker
 {
@@ -11,25 +16,54 @@ namespace FlightPathMarker
     {
         private const float WIDTH_ANGLE = 0.0004f;
         private const float CROSSHAIR_DISTANCE = 0.1f;
-        private const int AVERAGE_SAMPLES = 5;
+        private const int AVERAGE_SAMPLES = 10;
         private Vector3[] velocities = new Vector3[AVERAGE_SAMPLES];
         private int nextAverage = 0;
+        private IMyCubeGrid frameOfReference;
+        private List<IHitInfo> traceBuffer = new List<IHitInfo>();
+
+        public override void UpdateAfterSimulation()
+        {
+            if (PlayerShip == null)
+            {
+                return;
+            }
+
+            if (MyAPIGateway.Input.GetGameControl(MyControlsSpace.SECONDARY_TOOL_ACTION).IsNewPressed())
+            {
+                var head = ControlledEntity.GetHeadMatrix(true, true, false);
+
+                MyAPIGateway.Physics.CastRay(
+                    head.Translation + head.Forward * 10.0f,
+                    head.Translation + head.Forward * 10000.0f,
+                    traceBuffer);
+
+                frameOfReference = null;
+                foreach (IHitInfo info in traceBuffer) 
+                {
+                    if (info.HitEntity is IMyCubeGrid && info.HitEntity != PlayerShip) {
+                        frameOfReference = info.HitEntity as IMyCubeGrid;
+                        break;
+                    }
+                }
+            };
+        }
 
         public override void Draw()
         {
-            var physics = ShipPhysics();
-            if (physics == null)
+            if (PlayerCockpitPhysics == null)
             {
                 return;
             }
+            
+            var velocity = PlayerCockpitPhysics.LinearVelocity;
 
-            var camera = MyAPIGateway.Session?.Camera;
-            if (camera == null)
-            {
-                return;
+            if (frameOfReference?.Physics != null) {
+                velocity -= frameOfReference.Physics.LinearVelocity;
+                DrawBoundingSphere(frameOfReference, Color.LightGreen);
             }
-
-            var velocity = UpdateAverageVelocity(physics.LinearVelocity);
+            
+            velocity = UpdateAverageVelocity(velocity);
             if (Vector3.IsZero(velocity))
             {
                 return;
@@ -39,6 +73,17 @@ namespace FlightPathMarker
 
             DrawCrosshair(velocity, Color.LightGreen);
             DrawCrosshair(-velocity, Color.Red);
+        }
+
+        private void DrawBoundingSphere(IMyEntity entity, Color color) {
+            var matrix = entity.WorldMatrix;
+
+            MySimpleObjectDraw.DrawTransparentSphere(
+                    ref matrix,
+                    entity.LocalVolume.Radius,
+                    ref color,
+                    MySimpleObjectRasterizer.Wireframe,
+                    16);
         }
 
         private Vector3 UpdateAverageVelocity(Vector3 velocity)
@@ -59,33 +104,56 @@ namespace FlightPathMarker
         private void DrawCrosshair(Vector3 direction, Color color)
         {
             var camera = MyAPIGateway.Session?.Camera;
-            if (camera == null)
+            if (camera == null || ControlledEntity == null)
             {
                 return;
             }
 
+            var head = ControlledEntity.GetHeadMatrix(true, true, false);
+
             var width = camera.FovWithZoom * WIDTH_ANGLE;
-            
+
             MyTransparentGeometry.AddBillboardOriented(
                 material: "WhiteDot",
                 color: color,
-                origin: camera.WorldMatrix.Translation + direction * CROSSHAIR_DISTANCE,
-                leftVector: camera.WorldMatrix.Left,
-                upVector: camera.WorldMatrix.Up,
+                origin: head.Translation + direction * CROSSHAIR_DISTANCE,
+                leftVector: head.Left,
+                upVector: head.Up,
                 radius: width);
         }
 
-        private MyPhysicsComponentBase ShipPhysics()
-        {
-            var entity = MyAPIGateway.Session?.Player?.Controller?.ControlledEntity?.Entity;
-            if (entity == null)
-            {
-                return null;
-            }
 
-            return entity is IMyCubeBlock
-                ? entity.Parent.Physics
-                : null;
+        private MyPhysicsComponentBase PlayerCockpitPhysics
+        {
+            get
+            {
+                return PlayerCockpit?.Parent?.Physics;
+            }
+        }
+
+        private IMyCubeGrid PlayerShip
+        {
+            get
+            {
+                return PlayerCockpit?.CubeGrid;
+            }
+        }
+
+        private IMyCubeBlock PlayerCockpit
+        {
+            get
+            {
+                var entity = ControlledEntity?.Entity;
+                return (entity as IMyCubeBlock);
+            }
+        }
+
+        private IMyControllableEntity ControlledEntity
+        {
+            get
+            {
+                return MyAPIGateway.Session?.Player?.Controller?.ControlledEntity;
+            }
         }
     }
 }
