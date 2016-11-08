0d2b91e47c77c4e93bbf41ab551f6c0edd2c838d
David Garbutt <jayemen.85@gmail.com>
2016-11-07 22:33:48 -0500

    Finalize

--- Data/Scripts/FlightPathMarker/FlightPathMarker.cs
+++ Data/Scripts/FlightPathMarker/FlightPathMarker.cs
@@ -8,6 +8,7 @@ using VRage.ModAPI;
 using VRage.Game;
 using VRage.Game.ModAPI.Interfaces;
 using System.Collections.Generic;
+using VRage.Audio;
 
 namespace FlightPathMarker
 {
@@ -17,10 +18,9 @@ namespace FlightPathMarker
         private const float WIDTH_ANGLE = 0.0004f;
         private const float CROSSHAIR_DISTANCE = 0.1f;
         private const int AVERAGE_SAMPLES = 10;
-        private Vector3[] velocities = new Vector3[AVERAGE_SAMPLES];
-        private int nextAverage = 0;
-        private IMyCubeGrid frameOfReference;
+        private IMyCubeGrid target;
         private List<IHitInfo> traceBuffer = new List<IHitInfo>();
+        private AveragedVector averageVelocity = new AveragedVector(AVERAGE_SAMPLES);
 
         public override void UpdateAfterSimulation()
         {
@@ -38,13 +38,24 @@ namespace FlightPathMarker
                     head.Translation + head.Forward * 10000.0f,
                     traceBuffer);
 
-                frameOfReference = null;
-                foreach (IHitInfo info in traceBuffer) 
+                bool hadTarget = target != null;
+                target = null;
+                foreach (IHitInfo info in traceBuffer)
                 {
-                    if (info.HitEntity is IMyCubeGrid && info.HitEntity != PlayerShip) {
-                        frameOfReference = info.HitEntity as IMyCubeGrid;
+                    if (info.HitEntity is IMyCubeGrid && info.HitEntity != PlayerShip)
+                    {
+                        target = info.HitEntity as IMyCubeGrid;
                         break;
                     }
+                }
+
+                if (hadTarget && target == null)
+                {
+                    MyAPIGateway.Utilities.ShowNotification("Frame of reference cleared");
+                }
+                else if (target != null)
+                {
+                    MyAPIGateway.Utilities.ShowNotification("Frame of reference set");
                 }
             };
         }
@@ -55,15 +66,15 @@ namespace FlightPathMarker
             {
                 return;
             }
-            
+
             var velocity = PlayerCockpitPhysics.LinearVelocity;
 
-            if (frameOfReference?.Physics != null) {
-                velocity -= frameOfReference.Physics.LinearVelocity;
-                DrawBoundingSphere(frameOfReference, Color.LightGreen);
+            if (target?.Physics != null)
+            {
+                velocity -= target.Physics.LinearVelocity;
             }
-            
-            velocity = UpdateAverageVelocity(velocity);
+
+            velocity = averageVelocity.Update(velocity);
             if (Vector3.IsZero(velocity))
             {
                 return;
@@ -71,47 +82,19 @@ namespace FlightPathMarker
 
             velocity.Normalize();
 
-            DrawCrosshair(velocity, Color.LightGreen);
-            DrawCrosshair(-velocity, Color.Red);
+            DrawFlightPathMarker(velocity, Color.LightGreen);
+            DrawFlightPathMarker(-velocity, Color.Red);
         }
 
-        private void DrawBoundingSphere(IMyEntity entity, Color color) {
-            var matrix = entity.WorldMatrix;
-
-            MySimpleObjectDraw.DrawTransparentSphere(
-                    ref matrix,
-                    entity.LocalVolume.Radius,
-                    ref color,
-                    MySimpleObjectRasterizer.Wireframe,
-                    16);
-        }
-
-        private Vector3 UpdateAverageVelocity(Vector3 velocity)
+        private void DrawFlightPathMarker(Vector3 direction, Color color)
         {
-            velocities[nextAverage] = velocity;
-            nextAverage = (nextAverage + 1) % AVERAGE_SAMPLES;
-
-            var average = velocities[0];
-
-            for (var i = 1; i < AVERAGE_SAMPLES; ++i)
-            {
-                average += velocities[i];
-            }
-
-            return average / AVERAGE_SAMPLES;
-        }
-
-        private void DrawCrosshair(Vector3 direction, Color color)
-        {
-            var camera = MyAPIGateway.Session?.Camera;
-            if (camera == null || ControlledEntity == null)
+            if (Camera == null || ControlledEntity == null)
             {
                 return;
             }
 
-            var head = ControlledEntity.GetHeadMatrix(true, true, false);
-
-            var width = camera.FovWithZoom * WIDTH_ANGLE;
+            var head = ControlledEntity.GetHeadMatrix(true, true);
+            var width = Camera.FovWithZoom * WIDTH_ANGLE;
 
             MyTransparentGeometry.AddBillboardOriented(
                 material: "WhiteDot",
@@ -153,6 +136,14 @@ namespace FlightPathMarker
             get
             {
                 return MyAPIGateway.Session?.Player?.Controller?.ControlledEntity;
+            }
+        }
+
+        private IMyCamera Camera
+        {
+            get
+            {
+                return MyAPIGateway.Session?.Camera;
             }
         }
     }
