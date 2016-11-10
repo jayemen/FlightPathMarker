5bc8cf96202b7fdbea9abc23421553e342727227
David Garbutt <jayemen.85@gmail.com>
2016-11-09 20:11:22 -0500

    Clean up.

--- Data/Scripts/FlightPathMarker/FlightPathMarker.cs
+++ Data/Scripts/FlightPathMarker/FlightPathMarker.cs
@@ -7,8 +7,6 @@ using Sandbox.Game;
 using VRage.ModAPI;
 using VRage.Game;
 using VRage.Game.ModAPI.Interfaces;
-using System.Collections.Generic;
-using VRage.Audio;
 
 namespace FlightPathMarker
 {
@@ -19,35 +17,20 @@ namespace FlightPathMarker
         private const float CROSSHAIR_DISTANCE = 0.1f;
         private const int AVERAGE_SAMPLES = 10;
         private IMyCubeGrid target;
-        private List<IHitInfo> traceBuffer = new List<IHitInfo>();
-        private AveragedVector averageVelocity = new AveragedVector(AVERAGE_SAMPLES);
-
+        readonly private AveragedVector averageVelocity = new AveragedVector(AVERAGE_SAMPLES);
+        readonly private Util util = new Util();
+        
         public override void UpdateAfterSimulation()
         {
-            if (PlayerShip == null)
+            if (util.PlayerShip == null)
             {
                 return;
             }
 
             if (MyAPIGateway.Input.GetGameControl(MyControlsSpace.SECONDARY_TOOL_ACTION).IsNewPressed())
             {
-                var head = ControlledEntity.GetHeadMatrix(true, true, true, true);
-
-                MyAPIGateway.Physics.CastRay(
-                    head.Translation,
-                    head.Translation + (head.Forward * 100000.0f),
-                    traceBuffer);
-
                 bool hadTarget = target != null;
-                target = null;
-                foreach (IHitInfo info in traceBuffer)
-                {
-                    if (info.HitEntity is IMyCubeGrid && info.HitEntity != PlayerShip)
-                    {
-                        target = info.HitEntity as IMyCubeGrid;
-                        break;
-                    }
-                }
+                target = util.LookingAt<IMyCubeGrid>();
 
                 if (hadTarget && target == null)
                 {
@@ -62,19 +45,19 @@ namespace FlightPathMarker
 
         public override void Draw()
         {
-            if (PlayerCockpitPhysics == null)
-            {
+            if (!util.GameReady || util.PlayerShip == null) {
                 return;
             }
+            
+            var velocity = util.PlayerShip.Physics.LinearVelocity;
 
-            var velocity = PlayerCockpitPhysics.LinearVelocity;
-
-            if (target?.Physics != null)
+            if (target != null)
             {
                 velocity -= target.Physics.LinearVelocity;
             }
 
             velocity = averageVelocity.Update(velocity);
+            
             if (Vector3.IsZero(velocity))
             {
                 return;
@@ -85,24 +68,20 @@ namespace FlightPathMarker
             DrawFlightPathMarker(velocity, Color.LightGreen);
             DrawFlightPathMarker(-velocity, Color.Red);
 
-            if (MyAPIGateway.Input.GetGameControl(MyControlsSpace.SECONDARY_TOOL_ACTION).IsPressed()) {
-                var head = ControlledEntity.GetHeadMatrix(true, true, true, true);
+            if (MyAPIGateway.Input.GetGameControl(MyControlsSpace.SECONDARY_TOOL_ACTION).IsPressed())
+            {
+                var head = util.ControlledEntity.GetHeadMatrix(true, true, true);
+                var yellow = Color.Yellow;
 
                 head.Translation += head.Forward;
-                var color = Color.Yellow;
-                MySimpleObjectDraw.DrawTransparentSphere(ref head, .05f, ref color, MySimpleObjectRasterizer.SolidAndWireframe, 8);
+                MySimpleObjectDraw.DrawTransparentSphere(ref head, .05f, ref yellow, MySimpleObjectRasterizer.SolidAndWireframe, 8);
             }
         }
 
         private void DrawFlightPathMarker(Vector3 direction, Color color)
         {
-            if (Camera == null || ControlledEntity == null)
-            {
-                return;
-            }
-
-            var head = ControlledEntity.GetHeadMatrix(true, true, true, true);
-            var width = Camera.FovWithZoom * WIDTH_ANGLE;
+            var head = util.ControlledEntity.GetHeadMatrix(true, true, true);
+            var width = util.Camera.FovWithZoom * WIDTH_ANGLE;
 
             MyTransparentGeometry.AddBillboardOriented(
                 material: "WhiteDot",
@@ -111,48 +90,6 @@ namespace FlightPathMarker
                 leftVector: head.Left,
                 upVector: head.Up,
                 radius: width);
-        }
-
-
-        private MyPhysicsComponentBase PlayerCockpitPhysics
-        {
-            get
-            {
-                return PlayerCockpit?.Parent?.Physics;
-            }
-        }
-
-        private IMyCubeGrid PlayerShip
-        {
-            get
-            {
-                return PlayerCockpit?.CubeGrid;
-            }
-        }
-
-        private IMyCubeBlock PlayerCockpit
-        {
-            get
-            {
-                var entity = ControlledEntity?.Entity;
-                return (entity as IMyCubeBlock);
-            }
-        }
-
-        private IMyControllableEntity ControlledEntity
-        {
-            get
-            {
-                return MyAPIGateway.Session?.Player?.Controller?.ControlledEntity;
-            }
-        }
-
-        private IMyCamera Camera
-        {
-            get
-            {
-                return MyAPIGateway.Session?.Camera;
-            }
         }
     }
 }
---
+++ Data/Scripts/FlightPathMarker/Util.cs
@@ -0,0 +1,72 @@
+using Sandbox.ModAPI;
+using VRage.Game.Components;
+using VRage.Game.ModAPI;
+using VRage.ModAPI;
+using VRage.Game.ModAPI.Interfaces;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FlightPathMarker
+{
+    public class Util
+    {
+        private List<IHitInfo> traceBuffer = new List<IHitInfo>();
+        
+        public T LookingAt<T>()
+            where T : class, IMyEntity
+        {
+            var head = ControlledEntity.GetHeadMatrix(true, true, true);
+
+            MyAPIGateway.Physics.CastRay(
+                    from: head.Translation,
+                    to: head.Translation + (head.Forward * 100000.0f),
+                    toList: traceBuffer);
+
+            return traceBuffer
+                .Select(info => info.HitEntity)
+                .OfType<T>()
+                .Where(hit => hit != ControlledEntity && hit != PlayerShip)
+                .FirstOrDefault() as T;
+        }
+
+        public IMyCubeGrid PlayerShip
+        {
+            get
+            {
+                return PlayerCockpit?.CubeGrid;
+            }
+        }
+
+        public IMyCubeBlock PlayerCockpit
+        {
+            get
+            {
+                return (ControlledEntity.Entity as IMyCubeBlock);
+            }
+        }
+
+        public IMyControllableEntity ControlledEntity
+        {
+            get
+            {
+                return MyAPIGateway.Session.Player.Controller.ControlledEntity;
+            }
+        }
+
+        public IMyCamera Camera
+        {
+            get
+            {
+                return MyAPIGateway.Session.Camera;
+            }
+        }
+
+        public bool GameReady
+        {
+            get 
+            {
+                return MyAPIGateway.Utilities != null && MyAPIGateway.Session != null; 
+            }
+        }
+    }
+}
